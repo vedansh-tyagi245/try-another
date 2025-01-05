1350357b326e5aa39d604e75b10e5c2dc04c9ed6
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-05 23:03:13 +0530

    new2

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def add_name():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
